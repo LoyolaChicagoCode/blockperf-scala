00e7e17da0a1294b2c91ee52784c23eb29031d0c
George K. Thiruvathukal <gkt@cs.luc.edu>
2016-03-03 22:56:21 -0600

    updates to avoid name clashing with squants types
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- src/main/scala/blockperf/Performance.scala
+++ src/main/scala/blockperf/Performance.scala
@@ -3,26 +3,26 @@ import squants.storage._
 
 package object blockperf {
 
-  case class Time(t: Double) {
+  case class ElapsedTime(t: Double) {
     val time = Nanoseconds(t)
 
-    def +(another: Time): Time = Time(t + another.t)
+    def +(another: ElapsedTime): ElapsedTime = ElapsedTime(t + another.t)
 
     override def toString(): String = s"Time(${time.toString})"
 
   }
 
-  case class Space(m: Long) {
+  case class MemoryUsage(m: Long) {
     val used = Bytes(m) in Megabytes
     val free = Bytes(Runtime.getRuntime.freeMemory) in Gigabytes
     val total = Bytes(Runtime.getRuntime.totalMemory) in Gigabytes
 
-    def +(another: Space): Space = Space(m + another.m)
+    def +(another: MemoryUsage): MemoryUsage = MemoryUsage(m + another.m)
 
     override def toString(): String = s"Space(used=${used.toString}, free=${free.toString}, total=${total.toString})"
   }
 
-  def performance[R](block: => R): (Time, Space, R) = {
+  def performance[R](block: => R): (ElapsedTime, MemoryUsage, R) = {
     val t0 = System.nanoTime()
     val m0 = Runtime.getRuntime.freeMemory
 
@@ -32,6 +32,6 @@ package object blockperf {
     val m1 = Runtime.getRuntime.freeMemory
     val deltaT = t1 - t0
     val deltaM = m0 - m1
-    (Time(deltaT), Space(deltaM), result)
+    (ElapsedTime(deltaT), MemoryUsage(deltaM), result)
   }
 }
